2ba30707ab078f096f2640bf82885621270d204d
Alejandro Ordóñez <alejandro.ordonez@estudiant.upc.edu>
2018-12-20 13:30:44 +0100

    Product List: Imatges actualitzades 100% no fake

--- app/src/main/java/com/arnauac/vienaapp/ProductActivity/CategoryActivity.java
+++ app/src/main/java/com/arnauac/vienaapp/ProductActivity/CategoryActivity.java
@@ -40,47 +40,44 @@ public class CategoryActivity extends AppCompatActivity {
 
         if (text.equals("Coques")) {
             items = new ArrayList<>();
-            items.add(new ProductItem("Coques", R.drawable.cat_01_hamburgueses));
-            items.add(new ProductItem("item", R.drawable.cat_02_coques));
-            items.add(new ProductItem("Flautes", R.drawable.cat_03_flautes));
-            items.add(new ProductItem("Lleugers", R.drawable.cat_04_lleugers));
-            items.add(new ProductItem("Patates", R.drawable.cat_05_patates));
+            items.add(new ProductItem("Alpino", R.drawable.coq01_alpino));
+            items.add(new ProductItem("Remy", R.drawable.coq02_remy));
+            items.add(new ProductItem("Porchetta", R.drawable.coq03_porchetta));
+            items.add(new ProductItem("Lorraine", R.drawable.coq04_lorraine));
+
 
         }
         else if (text.equals("Hamburgueses")) {
             items = new ArrayList<>();
-            items.add(new ProductItem("Hamburgeses", R.drawable.cat_01_hamburgueses));
-            items.add(new ProductItem("item", R.drawable.cat_02_coques));
-            items.add(new ProductItem("Flautes", R.drawable.cat_03_flautes));
-            items.add(new ProductItem("Lleugers", R.drawable.cat_04_lleugers));
-            items.add(new ProductItem("Patates", R.drawable.cat_05_patates));
+            items.add(new ProductItem("Piri Piri", R.drawable.hamb01_piripiri));
+            items.add(new ProductItem("Oxbridge", R.drawable.hamb02_oxbridge));
+            items.add(new ProductItem("Cottage", R.drawable.hamb03_cottage));
+            items.add(new ProductItem("Tartufata", R.drawable.hamb04_tartufata));
+
 
         }
         else if (text.equals("Flautes")) {
             items = new ArrayList<>();
-            items.add(new ProductItem("Flautes", R.drawable.cat_01_hamburgueses));
-            items.add(new ProductItem("item", R.drawable.cat_02_coques));
-            items.add(new ProductItem("Flautes", R.drawable.cat_03_flautes));
-            items.add(new ProductItem("Lleugers", R.drawable.cat_04_lleugers));
-            items.add(new ProductItem("Patates", R.drawable.cat_05_patates));
+            items.add(new ProductItem("Strongonov", R.drawable.flauta01_strogonov));
+            items.add(new ProductItem("Britània", R.drawable.flauta02_britania));
+            items.add(new ProductItem("Ibèric de gla", R.drawable.flauta03_ibericagla));
+            items.add(new ProductItem("Norvègia", R.drawable.flauta04_norvegia));
+
 
         }
         else if (text.equals("Lleugers")) {
             items = new ArrayList<>();
-            items.add(new ProductItem("Lleugers", R.drawable.cat_01_hamburgueses));
-            items.add(new ProductItem("item", R.drawable.cat_02_coques));
-            items.add(new ProductItem("Flautes", R.drawable.cat_03_flautes));
-            items.add(new ProductItem("Lleugers", R.drawable.cat_04_lleugers));
-            items.add(new ProductItem("Patates", R.drawable.cat_05_patates));
+            items.add(new ProductItem("Amanida alla Norma", R.drawable.lle01_amanidanorma));
+            items.add(new ProductItem("Sucs naturals", R.drawable.lle02_sucs));
+            items.add(new ProductItem("Amanida Xató", R.drawable.lle03_xato));
+            items.add(new ProductItem("Sopa Galets", R.drawable.lle04_sopagalets));
+
 
         }
         else if (text.equals("Patates")) {
             items = new ArrayList<>();
-            items.add(new ProductItem("Patates", R.drawable.cat_01_hamburgueses));
-            items.add(new ProductItem("item", R.drawable.cat_02_coques));
-            items.add(new ProductItem("Flautes", R.drawable.cat_03_flautes));
-            items.add(new ProductItem("Lleugers", R.drawable.cat_04_lleugers));
-            items.add(new ProductItem("Patates", R.drawable.cat_05_patates));
+            items.add(new ProductItem("Uppsala", R.drawable.pat01_uppsala));
+
 
         }else{
             items = new ArrayList<>();
--- app/src/main/java/com/arnauac/vienaapp/fragments_MainActivity/MenuFragment/MenuFragment.java
+++ app/src/main/java/com/arnauac/vienaapp/fragments_MainActivity/MenuFragment/MenuFragment.java
@@ -52,8 +52,6 @@ public class MenuFragment extends Fragment {
         });
 
 
-
-
         return view;
     }
 }
