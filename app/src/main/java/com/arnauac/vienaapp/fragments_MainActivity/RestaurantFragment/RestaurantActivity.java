6d055c2846f9a34004fddade791b34caa40d1fda
alloza95 <naldusbros@gmail.com>
2018-12-24 22:06:46 +0100

    RestaurantActivity: - aplicar una proporció 16:9 a la imatge del restaurant - implementar el botó que et permet tornar a FragmentRestaurant - llista de serveis afegits (icone i text)

--- app/src/main/java/com/arnauac/vienaapp/fragments_MainActivity/RestaurantFragment/RestaurantActivity.java
+++ app/src/main/java/com/arnauac/vienaapp/fragments_MainActivity/RestaurantFragment/RestaurantActivity.java
@@ -1,17 +1,29 @@
 package com.arnauac.vienaapp.fragments_MainActivity.RestaurantFragment;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.arnauac.vienaapp.R;
 
 public class RestaurantActivity extends AppCompatActivity {
 
+    private LinearLayout linearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+
+        ImageView btn_back = findViewById(R.id.btn_back);
 
         TextView rName_view = findViewById(R.id.rName_view);
         TextView rStreet_view = findViewById(R.id.rStreet_view);
@@ -21,6 +33,8 @@ public class RestaurantActivity extends AppCompatActivity {
         TextView rTelephon_view = findViewById(R.id.rTelephon_view);
         TextView rHorary_view = findViewById(R.id.rHoraryDescription_view);
         TextView rDescription_view = findViewById(R.id.rDescription_view);
+
+        linearLayout = findViewById(R.id.services_linearLayout);
 
         RestaurantItem restaurant = (RestaurantItem) getIntent().getSerializableExtra("itemRestaurant");
 
@@ -32,5 +46,56 @@ public class RestaurantActivity extends AppCompatActivity {
         rTelephon_view.setText(restaurant.telephonRestaurant);
         rHorary_view.setText(restaurant.horaryRestaurant);
         rDescription_view.setText(restaurant.descriptionRestaurant);
+
+        if (restaurant.getServices().getWifi()){
+            createIcon(R.drawable.ic_wifi_connection_signal_symbol, "WIFI");
+        }
+        if (restaurant.getServices().getTerrace()){
+            createIcon(R.drawable.ic_sun, "TERRASSA");
+        }
+        if (restaurant.getServices().getAutoViena()){
+            createIcon(R.drawable.ic_car_fill_from_frontal_view, "AUTOVIENA");
+        }
+        if (restaurant.getServices().getParking()){
+            createIcon(R.drawable.ic_parking, "PARKING");
+        }
+        if (restaurant.getServices().getPlayground()){
+            createIcon(R.drawable.ic_playground, "PARC INFANTIL");
+        }
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void createIcon(int image, String text) {
+
+        LinearLayout vertical_LinearLayout = new LinearLayout(this);
+        LinearLayout.LayoutParams verLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        verLayoutParams.weight = 1;
+        vertical_LinearLayout.setLayoutParams(verLayoutParams);
+        vertical_LinearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(image);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(70, 70);
+        layoutParams.gravity = Gravity.CENTER;
+        imageView.setColorFilter(ContextCompat.getColor(this, R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+        imageView.setLayoutParams(layoutParams);
+
+        TextView textView = new TextView(this);
+        textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        textView.setGravity(Gravity.CENTER);
+        textView.setText(text);
+        textView.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        textView.setTextSize(10);
+
+        vertical_LinearLayout.addView(imageView);
+        vertical_LinearLayout.addView(textView);
+
+        linearLayout.addView(vertical_LinearLayout);
     }
 }
